71fdeee2aad8035762dd70052576600016b8c248
sjackson01 <stevenpjackson0@gmail.com>
2020-03-04 13:58:33 -0600

    Modify (loginout.php>Hunk 1): Update path.

--- inc/login_function.inc.php
+++ inc/login_function.inc.php
@@ -1,89 +1,81 @@
-<?php #script 12.2 - login_function.inc.php
-// This page defines two functions used by the login/logout process.container
+<?php # Script 13.8 - login_functions.inc.php #2
+// This page defines two functions used by the login/logout process.
 
-/* This function determines the absolute URL and redirects the user there.
- * The function takes one argument: the page  to be redireced to.
+/* This function determines an absolute URL and redirects the user there.
+ * The function takes one argument: the page to be redirected to.
  * The argument defaults to index.php.
- */ 
+ */
+function redirect_user($page = 'index.php') {
 
-function redirect_user($page = 'index.php'){
+	// Start defining the URL...
+	// URL is http:// plus the host name plus the current directory:
+	$url = 'http://' . $_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF']);
 
-    // Start defining the URL
-    // URL is http:// plus the host name plus the current directory:
-    $url = 'http://' . $_SERVER['HTTP_HOST']
-                     . dirname($_SERVER['PHP_SELF']);
- 
+	// Remove any trailing slashes:
+	$url = rtrim($url, '/\\');
 
-    // Remove any trailing slashes:
-    $url = rtrim($url,'/\\');
-    
-    // Add the page:
-    $url .= '/' . $page;
+	// Add the page:
+	$url .= '/' . $page;
 
-    // Redirect the user: 
-    header("Location: $url");
-    exit(); // Quit the script
+	// Redirect the user:
+	header("Location: $url");
+	exit(); // Quit the script.
 
-} // End of redirect_user() function
+} // End of redirect_user() function.
 
-/* This function validates the form data (the email address and password)
+
+/* This function validates the form data (the email address and password).
  * If both are present, the database is queried.
  * The function requires a database connection.
- * The funtion returns an array of information, including:
+ * The function returns an array of information, including:
  * - a TRUE/FALSE variable indicating success
  * - an array of either errors or the database result
  */
-
 function check_login($dbc, $email = '', $pass = '') {
-        
-    $errors = []; // Initialize error array.
-        
-    // Validate the email address:
-    if (empty($email)) {
-        $errors[] = 'You forgot to enter your email address.';
-    } else {
-        $e = mysqli_real_escape_string ($dbc, trim($email));
-    }
-        
-    // Validate the password:
-    if (empty($pass)) {
-    $errors[] = 'You forgot to enter your password.';
-    } else {
-    $p = mysqli_real_escape_string ($dbc, trim($pass));
-    }
 
-    if (empty($errors)) { // If everything's OK.
+	$errors = []; // Initialize error array.
 
-    // Retrieve the user_id and first_name for that email/password combination:
-    $q = "SELECT user_id, first_name
-        FROM users WHERE email='$e' AND
-        pass=SHA2('$p', 512)";
-    
-    $r = @mysqli_query($dbc, $q);
-        // Run the query.
+	// Validate the email address:
+	if (empty($email)) {
+		$errors[] = 'You forgot to enter your email address.';
+	} else {
+		$e = mysqli_real_escape_string($dbc, trim($email));
+	}
 
-    // Check the result:
-    if (mysqli_num_rows($r) == 1) {
+	// Validate the password:
+	if (empty($pass)) {
+		$errors[] = 'You forgot to enter your password.';
+	} else {
+		$p = trim($pass);
+	}
 
-        // Fetch the record:
-        $row = mysqli_fetch_array($r,
-            MYSQLI_ASSOC);
+	if (empty($errors)) { // If everything's OK.
 
-        // Return true and the record:
-        return [true, $row];
+		// Retrieve the user_id and first_name for that email/password combination:
+		$q = "SELECT user_id, first_name, FROM users WHERE email='$e'";
+		$r = @mysqli_query($dbc, $q); // Run the query.
 
-    } else { // Not a match!
-        $errors[] = 'The email address
-            and password entered do not
-            match those on file.';
-    }
+		// Check the result:
+		if (mysqli_num_rows($r) == 1) {
 
-    } // End of empty($errors) IF.
+			// Fetch the record:
+			$row = mysqli_fetch_array($r, MYSQLI_ASSOC);
 
-    // Return false and the errors:
-    return [false, $errors];
+			// Check the password:
+			if (password_verify($p, $row['pass'])) {
+				unset($row['pass']);
+				return [true, $row];
+			} else {
+				$errors[] = 'The email address and password entered do not match those on file.';
+			}
+
+		} else { // Not a match!
+			$errors[] = 'The email address and password entered do not match those on file.';
+		}
+
+	} // End of empty($errors) IF.
+
+	// Return false and the errors:
+	return [false, $errors];
 
 } // End of check_login() function.
-
- 
- 
